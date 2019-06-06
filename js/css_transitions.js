7845ef343015bf49ce414a8b405c10be5601bc0a
Anthony <aengland94@gmail.com>
2019-06-05 23:07:28 -0600

    added grid to rest of topics + started transitions topic

---
+++ js/css_transitions.js
@@ -0,0 +1,17 @@
+var dp1, dp2, dp3, dp4, dp5;
+
+function initListeners() {
+   dp1 = document.getElementById("dp1");
+   dp2 = document.getElementById("dp2");
+   dp3 = document.getElementById("dp3");
+   dp4 = document.getElementById("dp4");
+   dp5 = document.getElementById("dp5");
+}
+
+function showGraph() {
+   dp1.style.backgroundPositionX = "10%";
+   dp2.style.backgroundPositionX = "20%";
+   dp3.style.backgroundPositionX = "30%";
+   dp4.style.backgroundPositionX = "40%";
+   dp5.style.backgroundPositionX = "50%";
+}
